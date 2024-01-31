4a12b93ffc50800228c7fb954ff99c2922b556a2
Lalchand <dhaka916693@gmail.com>
2024-01-31 13:31:18 +0530

    slider image

---
+++ image-slide-main/script.js
@@ -0,0 +1,45 @@
+
+const slides = document.querySelectorAll('.slider');
+let counter = 0;
+
+slides.forEach((names, index) => {
+    names.style.left = `${index * 100}%`
+})
+
+ const myInterval = setInterval(()=>{
+    counter++
+    if(counter === slides.length){
+        counter =0;
+    }
+    sliderImage();
+}, 2000);
+
+
+
+const sliderImage = () => {
+    slides.forEach((names) => {
+        names.style.transform = `translateX(-${counter * 100}%)`
+    })
+}
+
+const goNext = () => {
+    counter++
+    if(counter == slides.length){
+        counter = 0;
+    }
+    sliderImage();
+}
+
+
+const goPrev = () => {
+    counter--
+    if(counter <0){
+        counter = slides.length-1;
+    }
+    sliderImage()
+}
+
+
+
+
+
